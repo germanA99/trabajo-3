26b0b187129ddbcb3c8f77902f91ce6344219147
germanA99 <darioalvaradogallegos@gmail.com>
2024-11-02 12:32:46 -0500

    trabajo de practica

---
+++ src/main/java/com/mycompany/mavenproject3/Mavenproject3.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ItsqmetEstudiantes
+ */
+public class Mavenproject3 {
+
+    public static void main(String[] args) {
+        
+        // Ejercicio cuatro
+        double calificacion;
+                
+        Scanner teclado = new Scanner(System.in);
+         
+        System.out.println("ingresar calificacion ");
+        calificacion = teclado.nextDouble();
+        
+        if ( calificacion > 90 && calificacion <=100){
+            System.out.println("el alumno tiene A");
+        }else if (calificacion >= 80 && calificacion < 90 ) {
+            System.out.println("el alumno tiene b");
+            
+            
+            // Ejercicio 7
+            
+          
+            double preciofinal;
+            double respueta
+            
+            Scanner teclado = new Scanner(System.in);
+            
+            System.out.println("ingresar el precion final");
+            precio = teclado.nextDouble();
+            
+            if ( precio > 100 && precio 20 %) {
+        } System.out.println("el cliente tiene 20%");
+        }
+               
+            
+
+        }
+    }
+}
